1f0d6c6769420ea3ddbda118bfaacc62912838f2
sarvani-16 <sarvanimanepalli16@gmail.com>
2026-06-15 17:26:43 +0530

    v2

--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,4 @@
-export const apibaseurl = "http://localhost:8000";
+export const apibaseurl = "https://gateway-r68x.onrender.com/docs";
 export const imgurl = import.meta.env.BASE_URL;
 export function callApi(reqMethod, apiUrl, jsonData, formData, responseHandler, jwtToken = "")
 {
